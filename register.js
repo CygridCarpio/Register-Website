3380c793db6e382ab09f2ece1ae7e53f26ee39fb
CygridCarpio <cygridcarpio78@gmail.com>
2024-03-05 21:38:34 +0800

    2nd comm

---
+++ register.js
@@ -0,0 +1,27 @@
+//console.log("Hello World!");
+//console.error("Error Happened");
+//console.warn("warning!!");
+
+// Variables
+// string = 'wordssss', '123'
+// number = '123, 43e, 2348.234
+// boolean= true, false
+// object = [apple, banana] [1,2,3]
+
+// typeof
+console.log(typeof(true))
+
+// Declaration of Variables
+let name = "wan",
+    lastname = "dere"
+    age = 100,
+    isProf = true,
+    hobbies = ["watching tv", "play video gmaes"];
+run = function () {
+    console.log("I'm running")
+};
+console.log(name, age, isProf, hobbies);
+console.table(hobbies);
+console.log("my name is" + name + " " + lastname);
+console.log(`my name is ${name} ${lastname}`);
+//console.log(name)
